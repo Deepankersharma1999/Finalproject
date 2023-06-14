1352340ed146a0903c76055b35666cb09d8aca96
Deepankersharma <Deepankersharma06@gmail.com>
2023-06-14 22:57:55 +0530

    Initial commit

---
+++ Js File/finalproject.js
@@ -0,0 +1,241 @@
+// for front page start....................................................>>>>>
+    $(document).ready(function () {
+      $(window).scroll(function () {
+        var positiontop = $(document).scrollTop();
+        console.log(positiontop);
+
+
+        if ((positiontop >2568) && (positiontop < 2768)) {
+          $('#card-one').addClass('animate__animated animate__backInLeft');
+          $('#card-two').addClass('animate__animated animate__backInRight');
+        }
+
+        if ((positiontop >3200) && (positiontop < 3700)) {
+          $('#card-three').addClass('animate__animated animate__backInLeft');
+          $('#card-four').addClass('animate__animated animate__backInRight');
+        }
+
+        if ((positiontop >3800) && (positiontop < 4200)) {
+          $('#card-fifth').addClass('animate__animated animate__backInLeft');
+          $('#card-sixth').addClass('animate__animated animate__backInRight');
+        }
+      })
+    })
+
+// for front page end..........................................................>>>>>> 
+
+
+
+
+//for footer front page start......................................................>>>>>>>>
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var positiontop = $(document).scrollTop();
+    console.log(positiontop);
+
+
+    if ((positiontop >4000) && (positiontop < 4500)) {
+      $('#footer-one').addClass('animate__animated animate__bounceIn');
+      $('#footer-two').addClass('animate__animated animate__bounceIn');
+      $('#footer-three').addClass('animate__animated animate__bounceIn');
+      $('#footer-four').addClass('animate__animated animate__bounceIn');
+    }
+  })
+})
+//for footer front page end........................................................>>>>>>>>
+
+
+// login me page start...................................................................>>>>>>>>>
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var positiontop = $(document).scrollTop();
+    console.log(positiontop);
+
+
+    if ((positiontop >5) && (positiontop < 80)) {
+      $('#loginme').addClass('animate__animated animate__slideInLeft');
+    
+    }
+  })
+})
+// login me page end  ...................................................................>>>>>>>>>
+
+//login me footer front page start......................................................>>>>>>>>
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var positiontop = $(document).scrollTop();
+    console.log(positiontop);
+
+
+    if ((positiontop >500) && (positiontop < 900)) {
+      $('#lo-f-one').addClass('animate__animated animate__bounceIn');
+      $('#lo-f-two').addClass('animate__animated animate__bounceIn');
+      $('#lo-f-three').addClass('animate__animated animate__bounceIn');
+      $('#lo-f-four').addClass('animate__animated animate__bounceIn');
+    }
+  })
+})
+//loginme footer front page end........................................................>>>>>>>>
+
+
+
+
+
+
+// testdrive page start...................................................................>>>>>>>>>
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var positiontop = $(document).scrollTop();
+    console.log(positiontop);
+
+
+    if ((positiontop >5) && (positiontop < 80)) {
+      $('#book').addClass('animate__animated animate__slideInLeft');
+    
+    }
+  })
+})
+// testdrive page end  ...................................................................>>>>>>>>>
+
+//test drive footer front page start......................................................>>>>>>>>
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var positiontop = $(document).scrollTop();
+    console.log(positiontop);
+
+
+    if ((positiontop >394) && (positiontop <400)) {
+      $('#b-f-one').addClass('animate__animated animate__bounceIn');
+      $('#b-f-two').addClass('animate__animated animate__bounceIn');
+      $('#b-f-three').addClass('animate__animated animate__bounceIn');
+      $('#b-f-four').addClass('animate__animated animate__bounceIn');
+    }
+  })
+})
+//testdrive footer front page end........................................................>>>>>>>>
+
+
+
+
+//About us footer front page start......................................................>>>>>>>>
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var positiontop = $(document).scrollTop();
+    console.log(positiontop);
+
+
+    if ((positiontop >3835) && (positiontop<4020)) {
+      $('#a-f-one').addClass('animate__animated animate__bounceIn');
+      $('#a-f-two').addClass('animate__animated animate__bounceIn');
+      $('#a-f-three').addClass('animate__animated animate__bounceIn');
+      $('#a-f-four').addClass('animate__animated animate__bounceIn');
+    }
+  })
+})
+//About us footer front page end........................................................>>>>>>>>
+
+
+
+
+
+
+
+//Our collection Brand page 1st img start......................................................>>>>>>>>
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var positiontop = $(document).scrollTop();
+    console.log(positiontop);
+
+
+    if ((positiontop >1700) && (positiontop<1900)) {
+      $('#b1').addClass('animate__animated animate__fadeInLeft');
+      $('#b2').addClass('animate__animated animate__fadeInLeft');
+      $('#b3').addClass('animate__animated animate__fadeInLeft');
+     
+    }
+  })
+})
+//our collection Brand page 1st img end........................................................>>>>>>>>
+
+
+
+
+//Our collection ev cars 1st img start......................................................>>>>>>>>
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var positiontop = $(document).scrollTop();
+    console.log(positiontop);
+
+
+    if ((positiontop >1700) && (positiontop<1900)) {
+      $('#ev1').addClass('animate__animated animate__fadeInRight');
+      $('#ev2').addClass('animate__animated animate__fadeInRight');
+      $('#ev3').addClass('animate__animated animate__fadeInRight');
+     
+    }
+  })
+})
+//our collection ev cars page 1st img end........................................................>>>>>>>>
+
+
+
+
+
+
+
+//Our collection amg cars 1st img start......................................................>>>>>>>>
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var positiontop = $(document).scrollTop();
+    console.log(positiontop);
+
+
+    if ((positiontop >2300) && (positiontop<2600)) {
+      $('#amg1').addClass('animate__animated animate__fadeInLeft');
+      $('#amg2').addClass('animate__animated animate__fadeInLeft');
+      $('#amg3').addClass('animate__animated animate__fadeInLeft');
+     
+    }
+  })
+})
+//our collection amg cars page 1st img end........................................................>>>>>>>>
+
+
+
+
+
+
+
+//Our collection Maybach cars 1st img start......................................................>>>>>>>>
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var positiontop = $(document).scrollTop();
+    console.log(positiontop);
+
+
+    if ((positiontop >2900) && (positiontop<3300)) {
+      $('#maybach1').addClass('animate__animated animate__fadeInRight');
+      $('#maybach2').addClass('animate__animated animate__fadeInRight');
+      $('#maybach3').addClass('animate__animated animate__fadeInRight');
+     
+    }
+  })
+})
+//our collection Maybach cars page 1st img end........................................................>>>>>>>>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
